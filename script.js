8fcf98475c1ab267b64bb2a3c2bd2d21d1de53f8
Rick van Sabben <hcvsabben@gmail.com>
2026-06-15 22:13:56 +0200

    Verberg wizard stappen bij start
    Removed event listener for DOMContentLoaded and added window.onload to hide specific elements on page load.

--- script.js
+++ script.js
@@ -1,49 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const cards = document.querySelectorAll(".card");
-    const drinkCard = cards[1];
-    const locationCard = cards[2];
-    const adviceCard = cards[3];
-
-    const buttons = Array.from(document.querySelectorAll("button"));
-    const scanButton = buttons.find(button => button.innerText.includes("Scan"));
-    const manualButton = buttons.find(button => button.innerText.includes("Handmatig"));
-    const searchButton = buttons.find(button => button.innerText.includes("Zoek"));
-
-    function hide(element) {
-        if (element) element.style.display = "none";
-    }
-
-    function show(element) {
-        if (element) element.style.display = "block";
-    }
-
-    hide(drinkCard);
-    hide(locationCard);
-    hide(adviceCard);
-
-    if (scanButton) {
-        scanButton.addEventListener("click", () => {
-            alert("Fotoherkenning komt later. Vul nu handmatig de gegevens in.");
-            show(drinkCard);
-            show(locationCard);
-            hide(adviceCard);
-        });
-    }
-
-    if (manualButton) {
-        manualButton.addEventListener("click", () => {
-            show(drinkCard);
-            show(locationCard);
-            hide(adviceCard);
-        });
-    }
-
-    if (searchButton) {
-        searchButton.addEventListener("click", () => {
-            show(adviceCard);
-        });
-    }
-});
 function toonDrankgegevens() {
   document.getElementById("drankgegevens").style.display = "block";
 }
@@ -55,3 +9,9 @@ function toonLocatie() {
 function toonAdvies() {
   document.getElementById("advies").style.display = "block";
 }
+
+window.onload = function() {
+  document.getElementById("drankgegevens").style.display = "none";
+  document.getElementById("locatie").style.display = "none";
+  document.getElementById("advies").style.display = "none";
+};
